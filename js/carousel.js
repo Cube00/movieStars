02f694bc613cbc2a72b8a0ce659f3f685dfa7ee6
Your Name <coller.vato@gmail.com>
2021-02-16 23:44:25 +0400

    min carousel

--- js/carousel.js
+++ js/carousel.js
@@ -4,6 +4,7 @@ const next = document.querySelectorAll(".next");
 const prew = document.querySelectorAll(".prew");
 let count = 0;
 let x = 100;
+
 const countDown = () => {
     if (count !== 0) {
         count--;
@@ -15,24 +16,36 @@ const increase = (leng) => {
     }
 };
 
-prew[0].addEventListener("click", () => {
-    countDown();
-    slider[0].style.transform = `translateX( -${count * x}%)`;
-});
+for (let i = 0; i < slider.length; i++) {
+    prew[i].addEventListener("click", () => {
+        countDown();
+        slider[i].style.transform = `translateX( -${count * x}%)`;
+    });
+    next[i].addEventListener("click", () => {
+        let leng = slider[i].children.length;
+        increase(leng);
+        slider[i].style.transform = `translateX( -${count * x}%)`;
+    });
+}
 
-next[0].addEventListener("click", () => {
-    let leng = slider[0].children.length;
-    increase(leng);
-    slider[0].style.transform = `translateX( -${count * x}%)`;
-});
+// prew[0].addEventListener("click", () => {
+//     countDown();
+//     slider[0].style.transform = `translateX( -${count * x}%)`;
+// });
 
-prew[1].addEventListener("click", () => {
-    countDown();
-    slider[1].style.transform = `translateX( -${count * x}%)`;
-});
+// next[0].addEventListener("click", () => {
+//     let leng = slider[0].children.length;
+//     increase(leng);
+//     slider[0].style.transform = `translateX( -${count * x}%)`;
+// });
 
-next[1].addEventListener("click", () => {
-    let leng = slider[1].children.length;
-    increase(leng);
-    slider[1].style.transform = `translateX( -${count * x}%)`;
-});
+// prew[1].addEventListener("click", () => {
+//     countDown();
+//     slider[1].style.transform = `translateX( -${count * x}%)`;
+// });
+
+// next[1].addEventListener("click", () => {
+//     let leng = slider[1].children.length;
+//     increase(leng);
+//     slider[1].style.transform = `translateX( -${count * x}%)`;
+// });
